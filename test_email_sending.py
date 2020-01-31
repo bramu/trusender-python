03bf8d7e68bd987ae046cb11947e1cbba1fdd9d2
praveenyoungind <praveenyoungind@gmail.com>
2020-01-31 16:57:06 +0530

    python package for trusender

---
+++ test_email_sending.py
@@ -0,0 +1,5 @@
+from trusender.client import Client
+
+client = Client("sq-sandbox-f351deae031b3bcbcb4903a935776dfa")
+
+client.send_email('registration_mail_for_buyer', 'praveenyoungind@gmail.com', [])
